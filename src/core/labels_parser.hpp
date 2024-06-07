ca045c93f0e6690fda203e2c6505f1ef04e723be
Yury Batenko <jurbat@evrone.com>
2024-06-07 09:28:30 +0300

    More consistent output

--- src/core/labels_parser.hpp
+++ src/core/labels_parser.hpp
@@ -14,7 +14,7 @@ static void parse_lbl_type(std::ifstream *labels_stream) {
         std::vector<std::string> line_arr = split_string(line, ' '); 
         int addr = std::stoi(line_arr[1], nullptr, 16);
         std::string name = line_arr[2].erase(0, 1); // remove front dot .
-        std::cout << "  0x" << addr << " " << name << "\n";
+        std::cout << "    " << ssprintf0x0000x(addr) << " " << name << "\n";
         PredefinedConstant pc = { .name = name, .value = addr };
         original_memory_image.constants.push_back(pc);       
     }
@@ -34,7 +34,7 @@ static void parse_sld_type(std::ifstream *labels_stream) {
         std::vector<std::string> line_arr = split_string(line, '|'); 
         if (line_arr[5] == "F") {
             int addr = std::stoi(line_arr[4]);
-            std::cout << "  0x" << addr << " " << line_arr[6] << "\n";
+            std::cout << "    " << ssprintf0x0000x(addr) << " " << line_arr[6] << "\n";
             PredefinedConstant pc = { .name = line_arr[6], .value = addr };
             original_memory_image.constants.push_back(pc);
         }        
@@ -44,7 +44,7 @@ static void parse_sld_type(std::ifstream *labels_stream) {
 }
 
 static void labels_parser(std::ifstream *labels_stream, std::string extension) {
-    std::cout << Colors::MAGENTA << "Parsing labels:" << "\n";
+    std::cout << "Parsing labels:" << Colors::MAGENTA << "\n";
 
     if (string_tolower(extension) == ".lbl") {
         parse_lbl_type(labels_stream);
@@ -53,4 +53,5 @@ static void labels_parser(std::ifstream *labels_stream, std::string extension) {
     } else {
         fail(string_format("Unknown labels file format: %s. Provide either '.lbl' or '.sld' formats!", extension.c_str()));
     }
+    std::cout << Colors::RESET;
 }
